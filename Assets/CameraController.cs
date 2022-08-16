83fe0048d5755044aaed13eecf6e9c00cd0a3859
rubensPC <rubendroogh@hotmail.nl>
2022-08-16 22:44:35 +0200

    Water shader WIP

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -19,6 +19,22 @@ public class CameraController : MonoBehaviour
         this.sensitivity += Input.mouseScrollDelta.y * .01f;
         this.sensitivity = Mathf.Clamp(this.sensitivity, 0, 1);
         this.sensitivityText.text = $"Camera sensitivity: {this.sensitivity.ToString()}";
+
+        if (Input.GetMouseButton(1))
+        {
+            rotation.x += Input.GetAxis("Mouse X") * 5f;
+            rotation.y -= Input.GetAxis("Mouse Y") * 5f;
+
+            rotation.x = Mathf.Repeat(rotation.x, 360);
+            rotation.y = Mathf.Clamp(rotation.y, -90, 90);
+
+            this.transform.rotation = Quaternion.Euler(rotation.y, rotation.x, 0);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
     void FixedUpdate()
@@ -51,22 +67,6 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftControl))
         {
             this.transform.position -= this.transform.up * sensitivity;
-        }
-
-        if (Input.GetMouseButton(1))
-        {
-            rotation.x += Input.GetAxis("Mouse X") * 10f;
-            rotation.y -= Input.GetAxis("Mouse Y") * 10f;
-
-            rotation.x = Mathf.Repeat(rotation.x, 360);
-            rotation.y = Mathf.Clamp(rotation.y, -90, 90);
-
-            this.transform.rotation = Quaternion.Euler(rotation.y, rotation.x, 0);
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.None;
         }
     }
 }
